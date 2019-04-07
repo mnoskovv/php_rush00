711e6958a9c7988419ccfa97b3c271761e564efa
mnoskovv <nikita01noskov@gmail.com>
2019-04-07 14:17:55 +0300

    bask total

--- bask.php
+++ bask.php
@@ -13,14 +13,19 @@
 <?php
 	ini_set('error_reporting', E_ALL);
 	ini_set('display_errors', 1);
-
+	$total = 0;
 	require_once('connection.php');
 	if (isset($_COOKIE["TestCookie"]))
 	{
 		$goods = explode("/", $_COOKIE["TestCookie"]);
 		for ($index = 0; $index < count($goods); $index++)
 		{    
-   			printf("<center>%s<br><center>",$goods[$index]);
+			$item = explode("@", $goods[$index]);
+   			if (isset($item[0]) && isset($item[1]))
+   			{
+   				printf("<center>%s %d<br><center>",$item[0], $item[1]);
+   				$total += $item[1];
+   			}
    		}
 	}
 	else
@@ -32,6 +37,7 @@
 		setcookie ("TestCookie", "", time() - 3600);
 		header('Location: index.php');
 	}
+	echo("total: ".$total);
 ?>
 <form action="" method="post">
 	 <?php
