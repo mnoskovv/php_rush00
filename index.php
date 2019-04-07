fe89669223312fc356111529f60d5d795342573d
mnoskovv <nikita01noskov@gmail.com>
2019-04-07 11:49:16 +0300

    norme

--- index.php
+++ index.php
@@ -27,14 +27,7 @@
 		{
 			echo "<a href='index.php?logout=true'>logout</a>";
 		}
-
-		if (isset($_GET["logout"]))
-		{
-			setcookie ("logged_in", "", time() - 3600);
-			setcookie ("login", "", time() - 3600);
-		}
 	?>
-	
 	<a href="bask.php">basket</a>
   </div>
 </div>
@@ -44,55 +37,61 @@
   <input type="submit" name="tabs" value="Tabs">
   <input type="submit" name="all" value="All">
  </form>
- <script>
-	 	function itemAdded()
-		{
-			alert("Товар был успешно добавлен в корзину!");
-		}
-	 </script>
 <?php
- require_once('connection.php');
- $sql = "SELECT * FROM `goods`";
- if (isset($_POST['phones']))
- {
-  $sql = "SELECT * FROM goods WHERE category = 'phones'";
- }
- if (isset($_POST['computers']))
- {
-  $sql = "SELECT * FROM goods WHERE category = 'computers'";
- }
- if (isset($_POST['tabs']))
- {
-  $sql = "SELECT * FROM goods WHERE category = 'tablets'";
- }
- if (isset($_POST['all']))
- {
-  $sql = "SELECT * FROM goods WHERE 1";
- }
- $res = mysqli_query($connect, $sql);
- $i = 0;
-while ($row = mysqli_fetch_array($res))
-{
-	$i++;
-	$name = $row["name"];
-	printf ("
-		<div id='%d' class = 'article'>
-		<div class = 'article-title'>%s</div>
-		 <div class = 'article-description'>%s</div>
-		 <img height =\"200px\" width = '200px' src=\"%s\">	 
-		 <form action = '' method = 'post'> 
-		 <input type='hidden' name = 'good' value = '%s'>
-		 <input type='hidden' name = 'price' value = '%d'>
-		 <input type = 'submit'  name = 'submit' class = 'buy' value = '%d₴' onclick = 'itemAdded()'>
-		 </form>
-		 </div>
-		 ", $i ,$row["name"], $row["description"], $row["image"], $row["name"], $row["price"], $row["price"]);
-}
-if (isset($_POST['submit']))
-{
-	$value = $_COOKIE["TestCookie"]."/".$_POST['good'];
-	setcookie ("TestCookie", $value);
-}
-?>	
+	require_once('connection.php');
+	$sql = "SELECT * FROM `goods`";
+	if (isset($_POST['phones']))
+	{
+		$sql = "SELECT * FROM goods WHERE category = 'phones'";
+	}
+	if (isset($_POST['computers']))
+	{
+		$sql = "SELECT * FROM goods WHERE category = 'computers'";
+	}
+	if (isset($_POST['tabs']))
+	{
+		$sql = "SELECT * FROM goods WHERE category = 'tablets'";
+	}
+	if (isset($_POST['all']))
+	{
+		$sql = "SELECT * FROM goods WHERE 1";
+	}
+	$res = mysqli_query($connect, $sql);
+	$i = 0;
+	while ($row = mysqli_fetch_array($res))
+	{
+		$i++;
+		$name = $row["name"];
+		printf ("
+			<div id='%d' class = 'article'>
+			<div class = 'article-title'>%s</div>
+			 <div class = 'article-description'>%s</div>
+			 <img height =\"200px\" width = '200px' src=\"%s\">	 
+			 <form action = '' method = 'post'> 
+			 <input type='hidden' name = 'good' value = '%s'>
+			 <input type='hidden' name = 'price' value = '%d'>
+			 <input type = 'submit'  name = 'submit' class = 'buy' value = '%d₴' onclick = 'itemAdded()'>
+			 </form>
+			 </div>
+			 ", $i ,$row["name"], $row["description"], $row["image"], $row["name"], $row["price"], $row["price"]);
+	}
+	if (isset($_POST['submit']))
+	{
+		$value = $_COOKIE["TestCookie"]."/".$_POST['good'];
+		setcookie ("TestCookie", $value);
+	}
+	if (isset($_GET["logout"]))
+	{
+		setcookie ("logged_in", "", time() - 3600);
+		setcookie ("login", "", time() - 3600);
+		header('Location: index.php');
+	}
+?>
+<script>
+	function itemAdded()
+	{
+		alert("Товар был успешно добавлен в корзину!");
+	}
+</script>
 </body>
 </html>
